ccecd4be179aada8b1ed1d78d35e6add774c1dce
Wilian Zanetti <wilianzanettisuconic@gmail.com>
2026-02-19 18:42:53 -0300

    atualização

--- BancoZanetti/Program.cs
+++ BancoZanetti/Program.cs
@@ -32,52 +32,64 @@ public class Program
 
     private static void Menu()
     {
-       
-        Console.WriteLine(" 1 - saldo");
-        Console.WriteLine(" 2 - saque");
-        Console.WriteLine(" 3 - deposito");
-        Console.WriteLine(" 4 - sair");
-        Console.WriteLine("Escolha uma opção:");
-        decimal opcao = decimal.Parse(Console.ReadLine());
-
-        decimal valorInicial = 100;
-
-
-
-        if (opcao == 1)
+        bool sair = false;
+        while (!sair)
         {
-            Console.WriteLine("Saldo:" + valorInicial);
-            Menu(); 
-        }
-        else if (opcao == 2)
-        {
-            Console.WriteLine("Digite o valor do saque:");
-            decimal valorSaque = decimal.Parse (Console.ReadLine());
-            var novosaldo = valorInicial - valorSaque;
-            
-            Console.WriteLine("Saque realizado com sucesso!");
-            Console.WriteLine($"Saldo atualizado:{novosaldo}");
-            Menu(); 
-        }
-        else if (opcao == 3)
-        {
-            Console.WriteLine("Digite o valor do deposito:");
-            decimal valorDeposito = decimal.Parse(Console.ReadLine());
-            var novosaldo = valorInicial + valorDeposito;
-            Console.WriteLine("Depósito realizado com sucesso!");
-            Console.WriteLine("Saldo atualizado:" + novosaldo);
-            Menu(); 
+            Console.WriteLine(" 1 - saldo");
+            Console.WriteLine(" 2 - saque");
+            Console.WriteLine(" 3 - deposito");
+            Console.WriteLine(" 4 - sair");
+            Console.Write("Escolha uma opção: ");
 
-        }
-        else if (opcao == 4)
-        {
-            Console.WriteLine("Saindo...");
-            LoginSenha();
-        }
-        else
-        {
-            Console.WriteLine("Opção inválida!");
-            Menu(); 
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out int opcao))
+            {
+                Console.WriteLine("Entrada inválida.");
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Saldo: " + valorAtual);
+                    break;
+                case 2:
+                    Console.Write("Digite o valor do saque: ");
+                    var saqueInput = Console.ReadLine();
+                    if (!decimal.TryParse(saqueInput, out decimal valorSaque) || valorSaque <= 0)
+                    {
+                        Console.WriteLine("Valor inválido.");
+                        break;
+                    }
+                    if (valorSaque > valorAtual)
+                    {
+                        Console.WriteLine("Saldo insuficiente.");
+                        break;
+                    }
+                    valorAtual -= valorSaque;
+                    Console.WriteLine("Saque realizado com sucesso!");
+                    Console.WriteLine($"Saldo atualizado: {valorAtual}");
+                    break;
+                case 3:
+                    Console.Write("Digite o valor do deposito: ");
+                    var depositInput = Console.ReadLine();
+                    if (!decimal.TryParse(depositInput, out decimal valorDeposito) || valorDeposito <= 0)
+                    {
+                        Console.WriteLine("Valor inválido.");
+                        break;
+                    }
+                    valorAtual += valorDeposito;
+                    Console.WriteLine("Depósito realizado com sucesso!");
+                    Console.WriteLine($"Saldo atualizado: {valorAtual}");
+                    break;
+                case 4:
+                    Console.WriteLine("Saindo...");
+                    sair = true;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
         }
 
     }
